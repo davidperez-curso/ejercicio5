b7dadc9fdacf057f5880a2ecd303f5316f6c9b53
davidperez-curso <davarmando@gmail.com>
2021-01-24 21:20:39 -0600

    1er commit

---
+++ scrpipt.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $(".img1").click(function (e) { 
+        e.preventDefault();
+        $(this).hide(600); 
+    });
+    
+    $(".img2").click(function (e) { 
+        e.preventDefault();
+        $(this).hide(600); 
+    });
+
+    $(".img3").click(function (e) { 
+        e.preventDefault();
+        $(this).hide(600); 
+    });
+
+    $(".img4").click(function (e) { 
+        e.preventDefault();
+        $(this).hide(600); 
+    });
+
+    $(".img5").click(function (e) { 
+        e.preventDefault();
+        $(this).hide(600); 
+    });
+
+    $(".img6").click(function (e) { 
+        e.preventDefault();
+        $(this).hide(600); 
+    });
+
+    $(".img7").click(function (e) { 
+        e.preventDefault();
+        $(this).hide(600); 
+    });
+
+    $(".img8").click(function (e) { 
+        e.preventDefault();
+        $(this).hide(600); 
+    });
+
+    $(".btn").click(function (e) { 
+        e.preventDefault();
+        $("div").show(600);
+    });
+});
